cfc70a362cd7eed7f019b3f9a65110a188f8819b
ramses indaldecio <ramsesindaldecio@Mac-mini-de-ramses.local>
2022-03-17 20:08:54 -0700

    validar extension

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -10,16 +10,29 @@ const cargarArchivo = (req, res = response) => {
   }
 
   const {archivo} = req.files;
+  const nombreCortado = archivo.name.split('.'); // el split es la funcion que va a separar el nombre y la extensíon
+  const extension = nombreCortado[nombreCortado.length - 1]; // me manda solo el tipo de extension
 
-  const uploadPath =path.join(  __dirname, '../uploads/', archivo.name);
+  //validar la extension
+  const extensionesValidas = ['png', 'jpg', 'jpeg', 'gif'];
+  if (!extensionesValidas.includes(extension)) {
+      return res.status(400).json({
+          msg: `La extension ${extension} no es permitida, solo de tipo:  ${extensionesValidas}`
+      });
+  }
 
-  archivo.mv(uploadPath, (err)=> {
-    if (err) {
-      return res.status(500).json({err});
-    }
+  res.json({extension});
 
-    res.json({msg: 'File uploaded to ' + uploadPath});
-  });
+
+//   const uploadPath =path.join(  __dirname, '../uploads/', archivo.name);
+
+//   archivo.mv(uploadPath, (err)=> {
+//     if (err) {
+//       return res.status(500).json({err});
+//     }
+
+//     res.json({msg: 'File uploaded to ' + uploadPath});
+//   });
 }
 
 module.exports = {
