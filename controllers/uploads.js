7394e9b8aaf3c27c0a18e73d6ec9c5801cf0f7c8
ramses indaldecio <ramsesindaldecio@Mac-mini-de-ramses.local>
2022-03-18 21:29:21 -0700

    creado carpetas de destino y subir archivos

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,39 +1,28 @@
 const {response} = require('express');
-const path = require('path'); //para crear los paths
+const { subirArchivo } = require('../helpers');
 
-const { v4: uuidv4 } = require('uuid');
 
-const cargarArchivo = (req, res = response) => {
+const cargarArchivo = async (req, res = response) => {
 
 
   if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
     res.status(400).json({msg: 'No hay archivos que subir.'});
     return;
   }
-
-  const {archivo} = req.files;
-  const nombreCortado = archivo.name.split('.'); // el split es la funcion que va a separar el nombre y la extensíon
-  const extension = nombreCortado[nombreCortado.length - 1]; // me manda solo el tipo de extension
-
-  //validar la extension
-  const extensionesValidas = ['png', 'jpg', 'jpeg', 'gif'];
-  if (!extensionesValidas.includes(extension)) {
-      return res.status(400).json({
-          msg: `La extension ${extension} no es permitida, solo de tipo:  ${extensionesValidas}`
+  
+  try {
+      //const nombre = await subirArchivo(req.files, ['txt', 'md'], 'textos');
+      const nombre = await subirArchivo(req.files, undefined, 'imgs');
+    
+      res.json({
+          nombre
       });
+      
+  } catch (msg) {
+      res.status(400).json({msg});
   }
 
-
-  const nombreTemp = uuidv4() + '.' + extension;
-  const uploadPath =path.join(  __dirname, '../uploads/', nombreTemp);
-
-  archivo.mv(uploadPath, (err)=> {
-    if (err) {
-      return res.status(500).json({err});
-    }
-
-    res.json({msg: 'File uploaded to ' + uploadPath});
-  });
+  
 }
 
 module.exports = {
--- models/server.js
+++ models/server.js
@@ -56,7 +56,8 @@ class Server{
         // Carga de archivos - fileupload 
         this.app.use(fileUpload({
         useTempFiles : true,
-        tempFileDir : '/tmp/'
+        tempFileDir : '/tmp/',
+        createParentPath: true
         }));
     }
 
